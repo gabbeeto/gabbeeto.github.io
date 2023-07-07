91407d29ab98dfc67f890ccce66f75829e5326b8
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-07-07 10:23:44 -0300

    add dark Mode support

---
+++ darkModeSwitcher.js
@@ -0,0 +1,22 @@
+let rootElement = document.querySelector(":root");
+document.querySelector("html").addEventListener("mouseover", changeText);
+
+let button = document.querySelector("aside > button");
+button.addEventListener("click", switchModes);
+
+if (matchMedia("(prefers-color-scheme: dark)")) {
+  rootElement.classList.toggle("dark");
+  button.innerText = "Switch To Light Mode";
+}
+
+function changeText() {
+  if (rootElement.classList[0] == "dark") {
+    button.innerText = "Switch To Light Mode";
+  } else {
+    button.innerText = "Switch To Dark Mode";
+  }
+}
+
+function switchModes() {
+  rootElement.classList.toggle("dark");
+}
