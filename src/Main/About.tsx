f065a57b0f7a7e6f6419aa30d4a3977aaab82919
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2024-10-28 16:16:31 -0300

    update info

--- src/Main/About.tsx
+++ src/Main/About.tsx
@@ -3,9 +3,9 @@ import {ReactElement} from "react";
 export default function About({containerStyle}:{containerStyle: string}): ReactElement{
 return (<section className={`${containerStyle} mt-2`}><h2 className="textSize-2">About me</h2>
 
-<p> I'm a spanish speaker who happened to learn english when I was a teenager. I'm a loser who want to get better at life. I'm currently developing a game in my free time and I'm usually busy on my job and finishing highschool </p>
-<span className="text-red-800 textSize-5 ">(this dude is an adult and didn't even finish highschool)</span>
-<p>I'm slowly making a horror game but it'll take time since I only have time available when I'm streaming on <a className="text-purple-800 textSize-4" href="https://www.twitch.tv/gabbeeto" target="_blank" >twitch</a></p>
+<p> I'm a spanish speaker who happened to learn english when I was a teenager. I tried to make websites, make games again and now I'm back to making websites all over again? </p>
+<p>I'm looking to get into the software industry as well so if you hire someone.. I'm available </p>
+<p>I only have time available when I'm streaming on <a className="text-purple-800 textSize-4" href="https://www.twitch.tv/gabbeeto" target="_blank" >twitch</a> but I'll definitely be on discord available</p>
 </section>)
 
 }
