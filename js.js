1dc65fb31d0fc0ba07a47e9c7716226d3992ff1a
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2025-01-07 21:10:28 -0300

    add multiple language

--- js.js
+++ js.js
@@ -1,1 +1,41 @@
-alert("hello")
+
+
+if(navigator.language.startsWith('es')){
+    document.querySelector("html").lang = 'es'
+}
+else{
+    document.querySelector("html").lang = 'en'
+}
+
+class MSG{
+constructor(id, messages){
+    this.obj = document.querySelector(`#${id}`)
+    this.eng = messages[0]
+    this.es = messages[1]
+
+    if(document.querySelector("html").lang == 'en'){
+    this.obj.innerText = this.eng
+    }
+    else{
+    this.obj.innerText = this.es
+    }
+}
+updt(){
+    if(document.querySelector("html").lang == 'en'){
+    this.obj.innerText = this.eng
+    }
+    else{
+    this.obj.innerText = this.es
+    }
+}
+
+}
+
+    if(document.querySelector("html").lang == 'en'){
+    document.title = `gabbeeto's website`
+    }
+    else{
+    document.title = `la pagina de gabriel`
+    }
+
+const tit = new MSG("title", ["About Gabbeeto", "Acerca de Gabbeeto"]);
