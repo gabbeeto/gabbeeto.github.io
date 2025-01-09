ce18dcc5d545305365138fded9d3e993fd7a9710
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2025-01-08 22:01:07 -0300

    improve the website a lil bit

--- js.js
+++ js.js
@@ -14,10 +14,10 @@ constructor(id, messages){
     this.es = messages[1]
 
     if(document.querySelector("html").lang == 'en'){
-    this.obj.innerText = this.eng
+    this.obj.innerHTML = this.eng
     }
     else{
-    this.obj.innerText = this.es
+    this.obj.innerHTML = this.es
     }
 }
 updt(){
@@ -38,4 +38,8 @@ updt(){
     document.title = `la pagina de gabriel`
     }
 
-const tit = new MSG("title", ["About Gabbeeto", "Acerca de Gabbeeto"]);
+const list = [
+    new MSG("title", ["About Gabbeeto", "Acerca de Gabbeeto"]),
+    new MSG("aboutMeDescription", [`I have basic experience dealing with software. I know how to make front-end websites and make games with programming languages like; <abbr title="Hypertext Markup Language"> HTML</abbr>, <abbr title="Cascading Style Sheets">CSS</abbr>, JavaScript and GDscript.`, `Tengo experiencia basica lideando con software. se como hacer sitios web front-end y hacer videojuegos con lenguajes de programacion como; <abbr title="Hypertext Markup Language"> HTML</abbr>, <abbr title="Cascading Style Sheets">CSS</abbr>, JavaScript y GDscript.`]),
+    new MSG("aboutMeDescription2", [`I have more than 6 years of experience interacting with the English language so I'm good enough to educate myself from other topics that are not related to language learning. Spanish is my first language but I've gotten all the software knowledge by interacting with English sources that are related to programming`, `tengo mas de 6 anios de experiencia interactuando con el idioma ingles asi que soy lo suficientemente bueno como para educarme sobre otros temas no relacionado al lenguaje usando ingles. espaniol es mi primer idioma pero aprendi todo lo relacionado a software consumiendo contenido en ingles`]),
+]
