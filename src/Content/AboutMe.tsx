bc1ece551720744fe4ea0ec6d77d189c878fbc93
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2025-03-18 16:33:33 -0300

    fill the rest of the content

---
+++ src/Content/AboutMe.tsx
@@ -0,0 +1,23 @@
+import Lang, {LangT} from "./../Lang";
+import Section from './../Section';
+
+export default function Main() {
+	return (<Section content={(<>
+		<h2>
+			<Lang en={(<>Experience:</>)}
+				es={(<>Experiencia:</>)}
+			/>
+		</h2>
+		<p>
+			<Lang en={(<>I have basic experience working with software. I know how to develop front-end websites and create games using programming languages such as <span title="Hypertext Markup Language">HTML</span>, <span title="Cascading Style Sheets">CSS</span>, JavaScript, and GDScript.</>)}
+				es={(<>Tengo experiencia básica trabajando con software. Sé como desarrollar sitios web front-end y crear videojuegos utilizando lenguajes de programación como <span title="Hypertext Markup Language">HTML</span>, <span title="Cascading Style Sheets">CSS</span>, JavaScript y GDScript.</>)}
+			/>
+		</p>
+<p>
+			<Lang en={(<>I have more than six years of experience interacting with the English language, so I’m proficient enough to learn about topics beyond language learning. Spanish is my first language, but I’ve acquired all my software knowledge through English-language programming resources.</>)}
+				es={(<>Tengo más de 6 años de experiencia interactuando con el idioma inglés, por lo que soy lo suficientemente bueno para aprender en inglés sobre otros temas no relacionado a los idiomas. El español es mi lengua materna, pero aprendí todo lo relacionado con software consumiendo contenido en inglés.</>)}
+			/>
+</p>
+	</>)} />)
+
+}
--- src/MainElement.tsx
+++ src/MainElement.tsx
@@ -1,27 +1,13 @@
 import Lang, {LangT} from "./Lang";
 import Section from './Section';
 import me from './assets/me.png';
+import WhatIlike from './Content/WhatIlike'
+import AboutMe from './Content/AboutMe'
 
 export default function Main() {
 	return (<main>
-		<Section content={(<>
-			<h2>
-				<Lang en={(<>What I like:</>)}
-							es={(<>Lo que me gusta:</>)}
-				 />
-			</h2>
-			<div>
-			<p>
-
-				<Lang en={(<>I like to push my limits when it comes to learning—I always strive to improve. I enjoy programming, math, and working with software in general. When I was a kid, I learned how to edit YouTube videos, so my passion for software started at an early age(even though I was not actually doing anything related to programming). I'm curious about almost everything.</>)}
-							es={(<>Me gusta superar mis límites cuando se trata de aprender; siempre busco mejorar. Disfruto de la programación, las matemáticas y trabajar con software en general. Cuando era niño, aprendí a editar videos de YouTube(incluso si no estaba haciendo nada relacionado a la programación en realidad). Así que mi pasión por el software comenzó a una edad temprana. Tengo curiosidad por casi todo.</>)}
-				 />
-
-			</p>
-			<img width='200px' height='100px' src={me} alt={LangT("a picture of me", "una foto de mi")}/>
-			</div>
-
-		</>)} />
+					<WhatIlike/>
+					<AboutMe/>
 	</main>)
 
 }
