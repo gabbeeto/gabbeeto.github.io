26f35b34e711511b95a43bf14a751731ff0eb435
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2025-03-18 17:55:38 -0300

    change algorithm to set initial language of website

--- src/main.tsx
+++ src/main.tsx
@@ -10,28 +10,41 @@ declare global {
 }
 
 
-if (navigator.language.startsWith('es')) {
+let spanishInWebsite: Boolean = false
+for (let currentLang of navigator.languages) {
+	console.log(currentLang)
+	console.log(currentLang.startsWith('es'))
+
+	if (currentLang.startsWith('es')) {
+		spanishInWebsite = true
+	}
+}
+
+console.log(spanishInWebsite)
+
+
+if (spanishInWebsite) {
 	document.querySelector("html")!.lang = 'es'
 }
 else {
 	document.querySelector("html")!.lang = 'en'
 }
 
-
+window.lang = document.querySelector("html")!.lang
 const rootElement = document.getElementById('root');
 
 if (!rootElement) {
-  throw new Error("Root element not found");
+	throw new Error("Root element not found");
 }
 
 
 const root = createRoot(rootElement);
 
-export  function renderHTML(): void {
-		root.render(<>
-			<Header></Header>
-			<Main></Main>
-		</>)
-	}
+export function renderHTML(): void {
+	root.render(<>
+		<Header></Header>
+		<Main></Main>
+	</>)
+}
 
 renderHTML()
